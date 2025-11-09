4f2b6f3aa8b283fcbbd9f1823b40e38c3cca72e9
weibao123 <50439038+weibao123@users.noreply.github.com>
2025-11-09 15:29:28 +0800

    Add new customer sites to CUSTOMER_SITES

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,23 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    hongniu: {
+        api: 'http://hongniuzy2.com/api.php/provide/vod/from/hnm3u8',
+        name: '红牛资源',
+    },
+    liangzi: {
+        api: 'http://cj.lziapi.com/api.php/provide/vod/from/lzm3u8',
+        name: '量子资源',
+    },
+    tiantang: {
+        api: 'http://vipmv.cc/api.php/provide/vod',
+        name: '天堂资源',
+    },
+    wolong: {
+        api: 'http://collect.wolongzyw.com/api.php/provide/vod',
+        name: '卧龙资源',
+    },
+    yinghua: {
+        api: 'http://m3u8.apiyhzy.com/api.php/provide/vod',
+        name: '樱花资源',
     }
 };
 
